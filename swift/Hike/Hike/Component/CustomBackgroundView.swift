74351b5351babba326d937c252416d0aa1c45137
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-10-06 00:40:01 +0800

    HikeApp: Add card view

---
+++ swift/Hike/Hike/Component/CustomBackgroundView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomBackgroundView.swift
+//  Hike
+//
+//  Created by John Rey on 10/6/23.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+		ZStack {
+			Color.customGreenDark
+				.cornerRadius(40)
+				.offset(y: 12)
+			
+			Color.customGreenLight
+				.cornerRadius(40)
+				.offset(y: 3)
+				.opacity(0.85)
+			
+			LinearGradient(
+				colors: [
+					.customGreenLight,
+					.customGreenMedium
+				],
+				startPoint: .top,
+				endPoint: .bottom
+			)
+			.cornerRadius(40)
+		}
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+}
