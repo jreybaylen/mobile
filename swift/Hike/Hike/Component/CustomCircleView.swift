cfdd506785fab4ee7822f100cd40fac288f2fb0e
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-10-10 20:13:53 +0800

    HikeApp: add MotionAnimationView

---
+++ swift/Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,45 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by John Rey on 10/10/23.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+	@State private var isAnimateGradient = false
+	
+    var body: some View {
+		ZStack {
+			Circle()
+				.fill(
+					LinearGradient(
+						colors: [
+							.customIndigoMedium,
+							.customSalmonLight
+						],
+						startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+						endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+					)
+				)
+				.onAppear {
+					withAnimation(
+						.linear(duration: 3.0).repeatForever(autoreverses: true)
+					) {
+						isAnimateGradient.toggle()
+					}
+				}
+				.frame(
+					width: 256,
+					height: 256
+			)
+			
+			MotionAnimationView()
+		}
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ swift/Hike/Hike/Component/MotionAnimationView.swift
@@ -0,0 +1,80 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by John Rey on 10/10/23.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+	@State private var isAnimating = false
+	@State private var randomCircle = Int.random(in: 1...12)
+	
+	func randomCoordinate() -> CGFloat {
+		return CGFloat.random(in: 0...256)
+	}
+	
+	func randomSize() -> CGFloat {
+		return CGFloat(Int.random(in: 4...80))
+	}
+	
+	func randomScale() -> CGFloat {
+		return CGFloat(Double.random(in: 0.1...2.0))
+	}
+	
+	func randomSpeed() -> Double {
+		return Double.random(in: 0.05...1.0)
+	}
+	
+	func randomDelay() -> Double {
+		return Double.random(in: 0...2)
+	}
+	
+    var body: some View {
+		ZStack {
+			ForEach(0...randomCircle, id: \.self) {
+				item in
+				
+				Circle()
+					.foregroundStyle(.white)
+					.opacity(0.25)
+					.frame(width: randomSize())
+					.position(
+						x: randomCoordinate(),
+						y: randomCoordinate()
+					)
+					.scaleEffect(isAnimating ? randomScale() : 1)
+					.onAppear(
+						perform: {
+							withAnimation(
+								.interpolatingSpring(
+									stiffness: 0.25,
+									damping: 0.25
+								)
+								.repeatForever()
+								.speed(randomSpeed())
+								.delay(randomDelay())
+							) {
+								isAnimating.toggle()
+							}
+						}
+					)
+			}
+		}
+		.frame(
+			width: 256,
+			height: 256
+		)
+		.mask(Circle())
+		.drawingGroup()
+    }
+}
+
+#Preview {
+	MotionAnimationView()
+		.background(
+			Circle()
+				.fill(.teal)
+		)
+}
