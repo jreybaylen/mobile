e03d7c5fad4a882b293c9d6be29194efaf943884
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-11-22 21:25:40 +0800

    TouchDownApp: app configuration with app icons

---
+++ swift/TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by John Rey on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreen()
+        }
+    }
+}
