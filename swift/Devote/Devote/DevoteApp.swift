31c4eaddbaa6366672bee92e4df30079a1bbf001
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-11-25 23:59:44 +0800

    DevoteApp: create devote app with core data config

---
+++ swift/Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by John Rey on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
